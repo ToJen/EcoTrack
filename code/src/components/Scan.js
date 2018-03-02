d0796a7acce2587fad204870d5d8b1d119ea37a0
Tomisin <tomisinjenrola@gmail.com>
2018-03-02 00:07:17 -0330

    Added camera module

--- code/src/components/Scan.js
+++ code/src/components/Scan.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import { View, Button, StyleSheet, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import Camera from 'react-native-camera';
 
 
 export default class ScanScreen extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {barcode: ''}
+  }
+
+  onBarCodeRead = (e) => this.setState({barcode: e.data})
+
   render() {
     return (
       <View style={styles.container}>
-        
-        <View style={styles.formButton}>
+        <Camera
+          style={styles.preview}
+          onBarCodeRead={this.onBarCodeRead}
+          ref={cam => this.camera = cam}
+          aspect={Camera.constants.Aspect.fill}
+        >
+          <Text style={styles.cameraData}>{this.state.barcode}</Text>
+        </Camera>
+        <Text>{'\n'}</Text>
+        {/*<View style={styles.formButton}>
           <Icon.Button
             onPress={ () => console.log('Scanning...') }
             backgroundColor="#9ECF62"
@@ -16,7 +33,7 @@ export default class ScanScreen extends Component {
           >
             <Text style={{color: '#fff'}}>Scan</Text>
           </Icon.Button>
-        </View>
+        </View>*/}
       </View>
     );
   }
@@ -38,5 +55,13 @@ const styles = StyleSheet.create({
   formButton: {
     alignItems: "center",
     justifyContent: "center"
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  cameraData: {
+    backgroundColor: 'white'
   }
 });
