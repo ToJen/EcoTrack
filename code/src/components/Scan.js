1b94b447e93ce3aa81b54a7396c324e921348fc9
Tomisin <tomisinjenrola@gmail.com>
2018-03-07 20:44:18 -0330

    Added scanning functionality

--- code/src/components/Scan.js
+++ code/src/components/Scan.js
@@ -1,41 +1,49 @@
 import React, { Component } from 'react';
 import { View, Button, StyleSheet, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
-import Camera from 'react-native-camera';
+import { BarCodeScanner, Permissions } from 'expo';
 
 
 export default class ScanScreen extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {barcode: ''}
+    this.state = { 
+      hasCameraPermission: null,
+      barcodeType: '',
+      barcodeData: null
+    }
   }
 
-  onBarCodeRead = (e) => this.setState({barcode: e.data})
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({
+      hasCameraPermission: status === 'granted'
+    });
+  }
+
+  handleBarCodeRead = ({ type, data }) => {
+    alert(`Barcode with type ${type} and data ${data} has been scanned!`);
+  }
+
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Camera
-          style={styles.preview}
-          onBarCodeRead={this.onBarCodeRead}
-          ref={cam => this.camera = cam}
-          aspect={Camera.constants.Aspect.fill}
-        >
-          <Text style={styles.cameraData}>{this.state.barcode}</Text>
-        </Camera>
-        <Text>{'\n'}</Text>
-        {/*<View style={styles.formButton}>
-          <Icon.Button
-            onPress={ () => console.log('Scanning...') }
-            backgroundColor="#9ECF62"
-            name="camera"
-          >
-            <Text style={{color: '#fff'}}>Scan</Text>
-          </Icon.Button>
-        </View>*/}
-      </View>
-    );
+    const { hasCameraPermission } = this.state;
+
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <BarCodeScanner
+            onBarCodeRead={this.handleBarCodeRead}
+            style={StyleSheet.absoluteFill}
+          />
+        </View>
+      );
+    }
   }
 }
 
